3239d2b03a343319ef2f7981f952b70a0b2e3f1f
Benoît Hubert <benoithubert@gmail.com>
2018-06-10 23:57:27 +0200

    Albums app

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react'
 import { Container } from 'semantic-ui-react'
-import TodoList from './TodoList'
-import TodoListFilter from './TodoListFilter'
+import AlbumList from './albums/AlbumList'
+import albums from './albums/albumsWithRatings'
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      tasks: [
-        { id: 1, done: true, description: 'Faire la vaisselle' }
-      ]
-    }
-  }
   render () {
     return (
       <Container>
-        <h1>TodoList</h1>
-        <TodoListFilter />
-        <TodoList tasks={this.state.tasks} />
+        <AlbumList albums={albums} />
       </Container>
     )
   }
