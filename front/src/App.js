b817110ce0dc4810b356a636c317f5ce6bc7c2d0
Yvan De paep <depaep@hotmail.fr>
2018-06-11 14:29:48 +0200

    exo

--- front/src/App.js
+++ front/src/App.js
@@ -8,16 +8,45 @@ class App extends Component {
     super(props)
     this.state = {
       tasks: [
-        { id: 1, done: true, description: 'Faire la vaisselle' }
-      ]
+        { id: 1, done: true, description: 'Faire la vaisselle' },
+        { id: 2, done: false, description: 'Sortir la poubelle' }
+      ],
+      filter: 'todo'
     }
+    this.handleChange = this.handleChange.bind(this)
   }
+  handleChange (e, radio) {
+    this.setState({ filter: radio.value })
+  }
+
   render () {
+    let tasks
+    const {filter} = this.state
+    //  if(filter === 'all') {
+    //    tasks= this.state.tasks
+    //  }
+    // else if (filter === 'done') {
+    //    tasks= this.state.tasks.filter(task => task.done)
+    //  }
+    //  else if(filter === 'todo') {
+    //    tasks=this.state.tasks.filter(task => ! task.done)
+    switch (filter) {
+      case 'all':
+        tasks = this.state.tasks
+        break
+      case 'done' :
+        tasks = this.state.tasks.filter(task => task.done)
+        break
+      case 'todo' :
+      default :
+        tasks = this.state.tasks.filter(task => !task.done)
+    }
+
     return (
       <Container>
         <h1>TodoList</h1>
-        <TodoListFilter />
-        <TodoList tasks={this.state.tasks} />
+        <TodoListFilter value={this.state.filter} handleChange={this.handleChange}/>
+        <TodoList tasks={tasks} />
       </Container>
     )
   }
