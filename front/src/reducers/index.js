4ff175d437eb63eb5165e629004e9aa051fb768d
Sylvain <kosc.sylvain@gmail.com>
2018-06-12 11:38:06 +0200

    add reducer

---
+++ front/src/reducers/index.js
@@ -0,0 +1,15 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT_VOTE':
+        const {id} = action
+        const {candidates} = state
+        const i = candidates.findIndex(candidate => candidate.id === id)
+        const candidatesCopy = [...candidates]
+        candidatesCopy[i].votes += 1
+       return {
+        candidates: candidatesCopy}
+    default:
+      return state
+  }
+}
+export default reducer
